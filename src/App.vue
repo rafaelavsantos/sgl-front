048e4c91564486e689ff09f919b9a8c17a16f6f5
Rafaela Santos <rafaelasantosminios@gmail.com>
2024-05-13 20:15:42 -0300

    feat: Retirando componentes e ações iniciais

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+:root {
+  --dark: #01080E;
+  --gold: #FCBA03;
+  --gold-dark: #faca44;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", Arial, Helvetica, sans-serif;
+  background-color: var(--dark);
+  color: var(--gold);
 }
 </style>
